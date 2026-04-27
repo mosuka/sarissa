d713f9dc12aed4c852da64cce16f15a81c30b07e
mosuka <mosuka@users.noreply.github.com>
2026-04-27 13:40:27 +0000

    deploy: 040cf360f119d9903c5019344900969cc4f898bb

--- ja/searcher-c2a407aa.js
+++ ja/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-812ca712.js',
+                path_to_root + 'searchindex-65e9e580.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c489a0ff.js',
+                path_to_root + 'searchindex-c4cae615.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
